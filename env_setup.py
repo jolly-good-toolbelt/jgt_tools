76bf32e32611219270d4da0aff24a2089ac3540a
Brad Brown <brad@bradsbrown.com>
2019-07-17 15:07:07 -0500

    Enhancement: streamline bootstrap install

--- env_setup.py
+++ env_setup.py
@@ -7,5 +7,10 @@ try:
 except ModuleNotFoundError:
     import subprocess
 
-    subprocess.check_call(["poetry", "install"])
+    # if jgt_tools is not importable, one of two things has happened:
+    # either you are inside a "naked" virtualenv,
+    # or you are not inside the virutalenv at all.
+    # Installing and calling `env-setup` via subcommand and `poetry run`
+    # will ensure the "right thing" happens in either situation.
+    subprocess.check_call(["poetry", "run", "pip", "install", "jgt_tools"])
     subprocess.check_call(["poetry", "run", "env-setup"])
